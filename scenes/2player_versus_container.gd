6af7c7819eded6dc27ed7e9af91e7d8c59ec2f19
Bryjamin <benjaminabryante@gmail.com>
2019-08-20 05:34:18 +0100

    Fixed bug where client was being connected to twice.
    Due to client.connect_to_url being called in the ready function of both 2playerversus and the 2playerversus container. On startup of the node 2 connections were being sent to the server on two seperate clients.  Essentially acting as two difference players

--- scenes/2player_versus_container.gd
+++ scenes/2player_versus_container.gd
@@ -1,20 +1,18 @@
 extends Control
 
-var client = WebSocketClient.new()
+var client
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    
+    client = WebSocketClient.new()
     $TwoPlayerVersus.hide()
     $TwoPlayerVersus.disable()
     $TwoPlayerVersus.set_process(false)
     $TwoPlayerVersus/Grid.set_process(false)
     
-    print(Connections.VERSUS_PLAYER_WEBSOCKET_STRING)
     client.connect_to_url(Connections.VERSUS_PLAYER_WEBSOCKET_STRING)
     client.connect("connection_failed", self, "_on_connection_error")
     client.connect("connection_established", self, "_on_connection_established")
-    pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
