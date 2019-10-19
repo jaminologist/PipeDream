759b499f196d416a4d10f3e193749673612bef18
Bryjamin <benjaminabryante@gmail.com>
2019-10-19 09:23:15 +0100

    Added autocert manager

--- cmd/main.go
+++ cmd/main.go
@@ -4,10 +4,20 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"golang.org/x/crypto/acme/autocert"
 )
 
 func main() {
+
+	//var httpsSrv *http.Server
+	mgr := autocert.Manager{
+		Prompt:     autocert.AcceptTOS,
+		HostPolicy: autocert.HostWhitelist("www.wthpd.com"),
+		Cache:      autocert.DirCache("certs"),
+	}
+
 	http.Handle("/", http.FileServer(http.Dir("./static")))
 	fmt.Println("Listening on 80...")
-	log.Fatal(http.ListenAndServe(":80", nil))
+	log.Fatal(http.ListenAndServe(":80", mgr.HTTPHandler(nil)))
 }
