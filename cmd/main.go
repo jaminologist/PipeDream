de22e4545f3ab6772e9e574f2bfffe931b10f216
Bryjamin <benjaminabryante@gmail.com>
2019-10-28 05:52:41 +0000

    Removed unused comments

--- cmd/main.go
+++ cmd/main.go
@@ -7,32 +7,7 @@ import (
 )
 
 func main() {
-
-	/*mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir("./static")))
-
-	certManager := autocert.Manager{
-		Prompt:     autocert.AcceptTOS,
-		Cache:      autocert.DirCache("/certs"),
-		HostPolicy: autocert.HostWhitelist("www.wthpd.com"),
-	}
-	server := &http.Server{
-		Addr:    ":443",
-		Handler: mux,
-		TLSConfig: &tls.Config{
-			GetCertificate: certManager.GetCertificate,
-		},
-	}*/
-
 	fmt.Println("Listening on 17000...")
-
-	//go func() {
-
 	http.Handle("/", http.FileServer(http.Dir("./static")))
 	log.Fatal(http.ListenAndServe(":17000", nil))
-	//}()
-
-	//fmt.Println("Hold up?")
-
-	//server.ListenAndServeTLS("", "")
 }
