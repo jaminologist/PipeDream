54a922dd7e8a8f60cb33d35b02441543cb39c9f9
Bryjamin <benjaminabryante@gmail.com>
2020-02-02 13:51:53 +0000

    PR-1: Added 5 boards to that need to be completed to beat the tutorials

--- multiplayer/game/tutorial.go
+++ multiplayer/game/tutorial.go
@@ -3,9 +3,7 @@ package game
 import (
 	"bryjamin.com/multiplayer/game/model"
 	"bryjamin.com/multiplayer/message"
-	"bryjamin.com/multiplayer/player"
 	"bryjamin.com/multiplayer/send"
-	"fmt"
 )
 
 //TutorialGame Displays a tutorial to a player
@@ -13,7 +11,6 @@ type TutorialGame struct {
 	inboundBoardInputCh chan *message.BoardInput
 	outboundMessageCh   chan *message.Message
 	sendNewBoardCh      chan bool
-	finishedCh          chan bool
 
 	board          *model.Board
 	tutorialBoards []*model.Board
@@ -33,19 +30,65 @@ func NewTutorialGame(outboundMessageCh chan *message.Message) *TutorialGame {
 	second.InsertPipe(1, 1, model.LPIPE, model.UP)
 	second.InsertPipe(1, 0, model.LPIPE, model.LEFT)
 
-	third := model.NewEmptyBoard(2, 2)
+	third := model.NewEmptyBoard(2, 5)
 	third.InsertPipe(0, 0, model.LPIPE, model.DOWN)
-	third.InsertPipe(0, 1, model.LPIPE, model.RIGHT)
-	third.InsertPipe(1, 1, model.LPIPE, model.UP)
-	third.InsertPipe(1, 0, model.LPIPE, model.LEFT)
+	third.InsertPipe(0, 1, model.LINE, model.RIGHT)
+	third.InsertPipe(0, 2, model.LINE, model.UP)
+	third.InsertPipe(0, 3, model.END, model.UP)
+	third.InsertPipe(1, 0, model.LPIPE, model.DOWN)
+	third.InsertPipe(1, 1, model.LINE, model.RIGHT)
+	third.InsertPipe(1, 2, model.LINE, model.UP)
+	third.InsertPipe(1, 3, model.LINE, model.UP)
+	third.InsertPipe(1, 4, model.END, model.UP)
+
+	fourth := model.NewEmptyBoard(3, 3)
+	fourth.InsertPipe(0, 0, model.LINE, model.DOWN)
+	fourth.InsertPipe(0, 1, model.LINE, model.RIGHT)
+	fourth.InsertPipe(0, 2, model.LINE, model.UP)
+	fourth.InsertPipe(1, 0, model.END, model.DOWN)
+	fourth.InsertPipe(1, 1, model.ENDEXPLOSION2, model.UP)
+	fourth.InsertPipe(1, 2, model.LINE, model.RIGHT)
+	fourth.InsertPipe(2, 0, model.LINE, model.UP)
+	fourth.InsertPipe(2, 1, model.LINE, model.LEFT)
+	fourth.InsertPipe(2, 2, model.LINE, model.UP)
+
+	fifth := model.NewEmptyBoard(5, 5)
+	fifth.InsertPipe(0, 0, model.LINE, model.DOWN)
+	fifth.InsertPipe(0, 1, model.LINE, model.RIGHT)
+	fifth.InsertPipe(0, 2, model.LPIPE, model.UP)
+	fifth.InsertPipe(0, 3, model.LINE, model.UP)
+	fifth.InsertPipe(0, 4, model.LPIPE, model.UP)
+
+	fifth.InsertPipe(1, 0, model.END, model.LEFT)
+	fifth.InsertPipe(1, 1, model.LINE, model.RIGHT)
+	fifth.InsertPipe(1, 2, model.LINE, model.UP)
+	fifth.InsertPipe(1, 3, model.LINE, model.LEFT)
+	fifth.InsertPipe(1, 4, model.LINE, model.UP)
+
+	fifth.InsertPipe(2, 0, model.LINE, model.DOWN)
+	fifth.InsertPipe(2, 1, model.LINE, model.RIGHT)
+	fifth.InsertPipe(2, 2, model.ENDEXPLOSION3, model.DOWN)
+	fifth.InsertPipe(2, 3, model.LINE, model.UP)
+	fifth.InsertPipe(2, 4, model.LINE, model.UP)
+
+	fifth.InsertPipe(3, 0, model.LINE, model.LEFT)
+	fifth.InsertPipe(3, 1, model.LINE, model.RIGHT)
+	fifth.InsertPipe(3, 2, model.LINE, model.UP)
+	fifth.InsertPipe(3, 3, model.LINE, model.LEFT)
+	fifth.InsertPipe(3, 4, model.LINE, model.UP)
+
+	fifth.InsertPipe(4, 0, model.LPIPE, model.LEFT)
+	fifth.InsertPipe(4, 1, model.LINE, model.RIGHT)
+	fifth.InsertPipe(4, 2, model.LINE, model.UP)
+	fifth.InsertPipe(4, 3, model.LINE, model.UP)
+	fifth.InsertPipe(4, 4, model.LPIPE, model.UP)
 
 	return &TutorialGame{
 		inboundBoardInputCh: make(chan *message.BoardInput),
 		outboundMessageCh:   outboundMessageCh,
-		finishedCh:          make(chan bool),
 		board:               &firstBoard,
 		tutorialBoards: []*model.Board{
-			&firstBoard, &second, &third,
+			&firstBoard, &second, &third, &fourth, &fifth,
 		},
 	}
 }
@@ -54,14 +97,10 @@ func (game *TutorialGame) SendBoardInput(input *message.BoardInput) {
 	game.inboundBoardInputCh <- input
 }
 
-//SendMessage receives and handles a player message
-func (game *TutorialGame) SendMessage(message *player.PlayerMessage) {
-	//game.inboundBoardInputCh <- input
-}
-
 func (g *TutorialGame) Run() {
 
 	g.board, g.tutorialBoards = g.tutorialBoards[0], g.tutorialBoards[1:]
+	g.board.UpdateBoardPipeConnectionsButNoNewPipes()
 
 	send.SendMessageToAll(&model.BlitzGameState{
 		BoardReports: []model.BoardReport{
@@ -74,44 +113,21 @@ func (g *TutorialGame) Run() {
 OuterLoop:
 	for {
 		select {
-		case isOver := <-g.finishedCh:
-
-			fmt.Println("Up here!")
-
-			if isOver {
-				gameState := model.BlitzGameState{
-					Score:  0,
-					IsOver: true,
-				}
-				send.SendMessageToAll(&gameState, g.outboundMessageCh)
-				break OuterLoop
-			}
 		case boardInput := <-g.inboundBoardInputCh:
-			//g.boardInputProcessor.processBoardInput(boardInput.X, boardInput.Y)
-
-			//UpdateBoardPipeConnectionsButNoNewPipes
 
 			g.board.RotatePipeClockwise(boardInput.X, boardInput.Y)
 			boardReports := g.board.UpdateBoardPipeConnectionsButNoNewPipes()
 
-			//bip.score += calculateScoreFromBoardReports(boardReports)
-
 			isOver := false
 
 			if g.board.IsEmpty() {
-
 				if len(g.tutorialBoards) == 0 {
 					isOver = true
-					//break
 				} else {
-					fmt.Printf("%+v", g.board)
 					g.board, g.tutorialBoards = g.tutorialBoards[0], g.tutorialBoards[1:]
 					newBoardReports := g.board.UpdateBoardPipeConnectionsButNoNewPipes()
 					newBoardReports[0].IsNewBoard = true
 					boardReports = append(boardReports, newBoardReports...)
-
-					//fmt.Printf("%+v", boardReports)
-					fmt.Printf("%+v", g.board)
 				}
 			}
 
@@ -120,6 +136,10 @@ OuterLoop:
 				IsOver:       isOver,
 			}
 			send.SendMessageToAll(&gameState, g.outboundMessageCh)
+
+			if isOver {
+				break OuterLoop
+			}
 		}
 	}
 
